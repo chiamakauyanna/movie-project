a2643813b0d32307996c75f82b6978dbfc4ab1e6
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-10-23 17:10:49 +0100

    Styling: styled the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,19 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div className="text-center">
-            <h1 className="text-5xl font-heading text-accent mb-4">
+        <div className="container max-w-none bg-secondary h-screen flex justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                 <h1 className="text-5xl font-heading text-accent font-bold mb-4">
                 Welcome to Movie App
             </h1>
-            <p className="text-lg">
+            <p className="text-lg text-textPrimary font-body mb-4">
                 Explore our vast collection of cinematic masterpieces.
             </p>
+            <button className="px-3 py-1 bg-gold font-heading rounded mx-auto font-bold ">
+                <Link to="/movies">View Site</Link>
+            </button>
+            </div>
         </div>
     );
 };
