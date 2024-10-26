bb72d09cd90ccb309a966a378ae599ba8afcb917
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-10-26 08:34:48 +0100

    minor: changed state name

--- src/components/GetMovieDetails.jsx
+++ src/components/GetMovieDetails.jsx
@@ -8,7 +8,7 @@ const imageUrl = "https://image.tmdb.org/t/p";
 
 const GetMovieDetails = () => {
     const [details, setDetails] = useState({});
-    const [credits, setCredits] = useState({});
+    const [casts, setCasts] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     let { id } = useParams();
@@ -25,12 +25,11 @@ const GetMovieDetails = () => {
                     `${apiUrl}/movie/${id}?api_key=${apiKey}`
                 );
 
-                 const creditsResponse = await axios.get(
-                     `${apiUrl}/movie/${id}/credits?api_key=${apiKey}`
-                 );
+                const castResponse = await axios.get(
+                    `${apiUrl}/movie/${id}/credits?api_key=${apiKey}`
+                );
                 setDetails(response.data);
-                setCredits(creditsResponse.data.cast);
-                console.log(creditsResponse.data.cast)
+                setCasts(castResponse.data.cast);
             } catch (error) {
                 if (error.response) {
                     // The request was made and the server responded with a status code
@@ -49,107 +48,114 @@ const GetMovieDetails = () => {
         getMovieDetails();
     }, [id]);
 
-  const  handleBackButton = () => {
-    navigate(-1)
-  }
+    const handleBackButton = () => {
+        navigate(-1);
+    };
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>{error}</p>;
     return (
         <div className="container max-w-none bg-primary text-textPrimary font-body px-4">
-                <div className="absolute z-10 p-6">
-                    <button onClick={handleBackButton}>Back</button>
-                </div>
-                <img
-                    src={`${imageUrl}/original/${details.backdrop_path}`}
-                    alt="movie backdrop"
-                    loading="lazy"
-                    className="relative h-screen w-screen"
-                />
-                <div className="absolute inset-0 bg-primary opacity-75 flex gap-8 items-center px-8">
-                    <div className="flex-initial ">
-                        <img
-                            src={`${imageUrl}/w300/${details.poster_path}`}
-                            alt="tvShow poster"
-                            loading="lazy"
-                            className="rounded"
-                        />
-                    </div>
-                    {/* Details title */}
-                    <div className="flex-1">
-                        <div>
-                            <h2 className="font-bold font-header text-3xl mb-3">
-                                {details.title} ({" "}
-                                {new Date(details.release_date).getFullYear()} )
-                            </h2>
-                        </div>
-
-                            {/* details genre */}
-                            <div className="-mt-2">
-                                <p>
-                                    {details.genres
-                                        ?.map((genre) => genre.name)
-                                        .join(", ") || "No genres available"}
-                                </p>
-                            </div>
-
-                        {/* details runtime */}
-                        <div>
-                            <p className="italic text-lg mt-3">{details.runtime} min</p>
-                        </div>
-
-                        <div>
-                        {/* Details Rating */}
-                            <div className="bg-gold flex gap-1 text-primary font-bold rounded px-2 w-32 items-center justify-center mt-3">
-                                <h3>Rating -</h3>
-                                <p>
-                                    {details.vote_average
-                                        ? details.vote_average.toFixed(1)
-                                        : "N/A"}
-                                </p>
-                            </div>
-
-                            {/* details language */}
-                            <div className="flex gap-1 mt-4">
-                                <h3 className="pt-2 font-heading font-bold text-accent">
-                                    Language -
-                                </h3>
-                                <p className=" pt-2">
-                                    {details.original_language}
-                                </p>
-                            </div>
-
-
-                            {/* Details tagline */}
-                            <div>
-                                <h3 className="pt-2 font-heading font-bold text-accent">
-                                    Tagline
-                                </h3>
-                                <p className=" italic font-heading">
-                                    {details.tagline}
-                                </p>
-                            </div>
-
-                            {/* Details Overview */}
-                            <div>
-                                <h3 className="pt-4 font-heading font-bold text-accent">
-                                    Overview
-                                </h3>
-                                <p className=" pt-2 text-base">
-                                    {details.overview}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            {/* Credits */}
-            <div className="bg-secondary h-3">
-                {credits.cast?.map((cast) => 
-              <p>{cast.name}</p>
-            )}
+            <div className="absolute z-10 p-6">
+                <button onClick={handleBackButton}>Back</button>
             </div>
+            <img
+                src={`${imageUrl}/original/${details.backdrop_path}`}
+                alt="movie backdrop"
+                loading="lazy"
+                className="relative h-screen w-screen"
+            />
+            <div className="absolute inset-0 bg-primary opacity-75 flex gap-8 items-center px-8">
+                <div className="flex-initial ">
+                    <img
+                        src={`${imageUrl}/w300/${details.poster_path}`}
+                        alt="tvShow poster"
+                        loading="lazy"
+                        className="rounded"
+                    />
+                </div>
+                {/* Details title */}
+                <div className="flex-1">
+                    <div>
+                        <h2 className="font-bold font-header text-3xl mb-3">
+                            {details.title} ({" "}
+                            {new Date(details.release_date).getFullYear()} )
+                        </h2>
+                    </div>
+
+                    {/* details genre */}
+                    <div className="-mt-2">
+                        <p>
+                            {details.genres
+                                ?.map((genre) => genre.name)
+                                .join(", ") || "No genres available"}
+                        </p>
+                    </div>
+
+                    {/* details runtime */}
+                    <div>
+                        <p className="italic text-lg mt-3">
+                            {details.runtime} min
+                        </p>
+                    </div>
+
+                    <div>
+                        {/* Details Rating */}
+                        <div className="bg-gold flex gap-1 text-primary font-bold rounded px-2 w-32 items-center justify-center mt-3">
+                            <h3>Rating -</h3>
+                            <p>
+                                {details.vote_average
+                                    ? details.vote_average.toFixed(1)
+                                    : "N/A"}
+                            </p>
+                        </div>
+
+                        {/* details language */}
+                        <div className="flex gap-1 mt-4">
+                            <h3 className="pt-2 font-heading font-bold text-accent">
+                                Language -
+                            </h3>
+                            <p className=" pt-2">{details.original_language}</p>
+                        </div>
+
+                        {/* Details tagline */}
+                        <div>
+                            <h3 className="pt-2 font-heading font-bold text-accent">
+                                Tagline
+                            </h3>
+                            <p className=" italic font-heading">
+                                {details.tagline}
+                            </p>
+                        </div>
+
+                        {/* Details Overview */}
+                        <div>
+                            <h3 className="pt-4 font-heading font-bold text-accent">
+                                Overview
+                            </h3>
+                            <p className=" pt-2 text-base">
+                                {details.overview}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* cast */}
+            <ul className="bg-accent text-textPrimary my-8 mx-6 flex gap-2 truncate overflow-x-auto">
+                {casts.map((cast) => (
+                    <li key={cast.id} className="bg-secondary pr-1">
+                        <img
+                            src={`${imageUrl}/w92/${cast.profile_path}`}
+                            alt=""
+                        />
+                        <p className="text-sm">{cast.name}</p>
+                        <p>as</p>
+                        <p>{cast.character}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
-            
     );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,17 +2,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className="container max-w-none bg-secondary h-screen flex justify-center items-center">
-            <div className="flex flex-col justify-center items-center">
-                 <h1 className="text-5xl font-heading text-accent font-bold mb-4">
-                Welcome to Movie App
-            </h1>
-            <p className="text-lg text-textPrimary font-body mb-4">
-                Explore our vast collection of cinematic masterpieces.
-            </p>
-            <button className="px-3 py-1 bg-gold font-heading rounded mx-auto font-bold ">
-                <Link to="/movies">View Site</Link>
-            </button>
+        <div
+            className="container max-w-none flex justify-center items-center bg-cover bg-center h-screen bg-[url('https://nkpremices.com/content/images/size/w1600/2021/08/mih10uhu1464fx1kr0by-2.jpg')] relative"
+        >
+            <div className="bg-black absolute inset-0 opacity-50"></div>
+            <div className="flex flex-col justify-center items-center z-10">
+                <h1 className="text-5xl font-heading text-accent font-bold mb-4">
+                    Welcome to Movie App
+                </h1>
+                <p className="text-lg text-textPrimary font-body mb-4">
+                    Explore our vast collection of cinematic masterpieces.
+                </p>
+                <button className="px-3 py-1 bg-gold font-heading rounded mx-auto font-bold ">
+                    <Link to="/movies">View Site</Link>
+                </button>
             </div>
         </div>
     );
